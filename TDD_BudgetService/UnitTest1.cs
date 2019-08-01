751fd9bb8b1399b24ec342f5d03a4397644dbe55
yu-lang.chu <yu-lang.chu@redheads.de>
2019-08-01 10:43:49 +0800

    refactor test case

--- TDD_BudgetService/UnitTest1.cs
+++ TDD_BudgetService/UnitTest1.cs
@@ -5,17 +5,24 @@ namespace Tests
 {
   public class BudgetServiceTests
   {
+    private BudgetService service;
+
     [SetUp]
     public void Setup()
     {
+      service = new BudgetService();
     }
 
     [Test]
     public void No_Budget()
     {
-      var service = new BudgetService();
       var start = new DateTime(2019, 04, 01);
       var end = new DateTime(2019, 04, 03);
+      TotalBudgetShouldBe(start, end);
+    }
+
+    private void TotalBudgetShouldBe(DateTime start, DateTime end)
+    {
       decimal actual = service.Query(start, end);
       var expected = 0;
       Assert.AreEqual(expected, actual);
