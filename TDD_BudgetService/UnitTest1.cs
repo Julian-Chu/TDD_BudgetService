059ab69b80c90231b343cb30eaa5d1e75286863c
yu-lang.chu <yu-lang.chu@redheads.de>
2019-08-01 16:57:46 +0800

    refactor

--- TDD_BudgetService/UnitTest1.cs
+++ TDD_BudgetService/UnitTest1.cs
@@ -20,11 +20,8 @@ namespace Tests
     [Test]
     public void No_Budget()
     {
-      var budgets = new List<Budget>() { };
-      _repo.GetAll().Returns(budgets);
-      var start = new DateTime(2019, 04, 01);
-      var end = new DateTime(2019, 04, 03);
-      decimal actual = _service.Query(start, end);
+      _repo.GetAll().Returns(new List<Budget>() { });
+      decimal actual = _service.Query(new DateTime(2019, 04, 01), new DateTime(2019, 04, 03));
       var expected = 0;
       TotalBudgetShouldBe(expected, actual);
     }
@@ -32,17 +29,14 @@ namespace Tests
     [Test]
     public void Period_Inside_Budget_Month()
     {
-      var budgets = new List<Budget>() { new Budget() { YearMonth = "201904", Amount = 30 } };
-      _repo.GetAll().Returns(budgets);
+      _repo.GetAll().Returns(new List<Budget>() { new Budget() { YearMonth = "201904", Amount = 30 } });
 
-      var start = new DateTime(2019, 04, 01);
-      var end = new DateTime(2019, 04, 01);
-      decimal actual = _service.Query(start, end);
+      decimal actual = _service.Query(new DateTime(2019, 04, 01), new DateTime(2019, 04, 01));
       var expected = 1;
       TotalBudgetShouldBe(expected, actual);
     }
 
-    private void TotalBudgetShouldBe(int expected, decimal actual)
+    private void TotalBudgetShouldBe(decimal expected, decimal actual)
     {
       Assert.AreEqual(expected, actual);
     }
